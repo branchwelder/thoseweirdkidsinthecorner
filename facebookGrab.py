006fa6f35cb33ecb2a5993f7f08a382506df47b8
buttegab <gabriel.butterick@students.olin.edu>
2016-03-26 11:51:10 -0400

    made the token last longer probably

--- facebookGrab.py
+++ facebookGrab.py
@@ -9,11 +9,27 @@ def facebookGrab(profile):
 
 	results = []
 	access_token = os.environ.get('facebookAcessToken')
+	# access_token = 'CAABqy6QtDmwBAIlvxqiSRog7TtqWzLDL8RRtabQ4Sjaic6XWZCbcY099wIeaVDXbEsJJZCfZB56hpBBumTx95JPqSEfKryKNY6ZBzHMQZCn0e47Wws1sZA6aZA18Sluv0iAqDwVCXZBZAqskdCdFRIl2wJRYzrIm6rnexIW10ivGBTkXgaIZCcXZBFVdR6F09PYZAOBlZB2XhHT0Kn0MZApekKZCfRT'
 	user = profile
 
 	graph = facebook.GraphAPI(access_token)
+
+	# app_id = '117422865321580' # Obtained from https://developers.facebook.com/
+	# app_secret = '7e977218d4526ea175991deeeaefc589' # Obtained from https://developers.facebook.com/
+	app_id = os.environ.get('facebookAppId')
+	app_secret = os.environ.get('facebookAppSecret')
+
+
+	# Extend the expiration time of a valid OAuth access token.
+	extendedAccessToken = graph.extend_access_token(app_id, app_secret)
+	# graph = facebook.GraphAPI(extendedAccessToken)
+
 	profile = graph.get_object(user)
 	posts = graph.get_connections(profile['id'], 'posts')
+
+	# graph = facebook.GraphAPI(user_short_lived_token_from_client)
+
+
 
 	# Wrap this block in a while loop so we can keep paginating requests until
 	# finished.
